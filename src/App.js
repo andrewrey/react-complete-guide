728b5895fc538ffd8d4232ceea1fc365a54fd8bc
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-09 12:11:33 -0700

    update state using set state

--- src/App.js
+++ src/App.js
@@ -10,12 +10,22 @@ class App extends Component {
       { name: "Ryan", age: 33 },
     ],
   };
+  switchNameHandler = () => {
+    // DON'T DO THIS: this.state.persons[0].name = "Mike";
+    this.setState({
+      persons: [
+        { name: "Tony", age: 99 },
+        { name: "Bobby", age: 83 },
+        { name: "Timmy", age: 22 },
+      ],
+    });
+  };
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
           My Hobbies: Racing
