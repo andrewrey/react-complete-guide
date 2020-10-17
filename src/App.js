bbbaa156590d378f91b93c68e7f5f9aed73ab582
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-17 16:16:20 -0700

    added syltedComponents

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
-import Radium, { StyleRoot } from "radium";
+import Radium from "radium";
 
 class App extends Component {
   state = {
@@ -99,16 +99,14 @@ class App extends Component {
       classes.push("bold");
     }
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(" ")}>This is really working!</p>
-          <button style={style} onClick={this.togglePersonsHandler}>
-            Toggle Persons
-          </button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(" ")}>This is really working!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
+      </div>
     );
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,24 +1,40 @@
 import React from "react";
-import Radium from "radium";
-import "./Person.css";
+// import "./Person.css";
+import styled from "styled-components";
 
 const Person = ({ name, age, children, deleteName, nameChange }) => {
-  const style = {
-    "@media (min-width: 700px)": {
-      backgroundColor: "orange",
-      width: "450px",
-      color: "blue",
-    },
-  };
+  // const style = {
+  //   "@media (min-width: 700px)": {
+  //     backgroundColor: "orange",
+  //     width: "450px",
+  //     color: "blue",
+  //   },
+  // };
+  const StyledDiv = styled.div`
+    width: 60%;
+    margin: 16px auto;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    color: white;
+    background-color: orange;
+    @media (min-width: 700px) {
+      background-color: purple;
+      width: 450px;
+      color: blue;
+    }
+  `;
   return (
-    <div className="Person" style={style}>
+    // <div className="Person" style={style}>
+    <StyledDiv>
       <p onClick={deleteName}>
         My name is {name} and I am {age} years old.
       </p>
       <p>{children}</p>
       <input type="text" onChange={nameChange} value={name} />
-    </div>
+    </StyledDiv>
   );
 };
 
-export default Radium(Person);
+export default Person;
