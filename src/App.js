456b92a6d469ef03590e9664b436133cf49e9d15
Andrew Reynolds <andrew_reynolds@mac.com>
2020-10-18 17:33:24 -0700

    removed styled components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
-import styled from "styled-components";
+// import styled from "styled-components";
 // import Radium from "radium";
-
-const StyledButton = styled.button`
-  background-color: ${(props) => (props.alt ? "red" : "green")};
-  color: white;
-  font: inherit;
-  border: 1px solid yellow;
-  padding: 8px;
-  cursor: pointer;
-  boxshadow: 2px 2px 3px black;
-
-  &:hover {
-    background-color: ${(props) => (props.alt ? "orange" : "blue")};
-    color: black;
-  }
-`;
 
 class App extends Component {
   state = {
@@ -118,9 +103,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(" ")}>This is really working!</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
+        <button className="button" onClick={this.togglePersonsHandler}>
           Toggle Persons
-        </StyledButton>
+        </button>
         {persons}
       </div>
     );
